ba5e9c23b148cbcdcb8dfe6edea2dc982c4b4c46
James R <Jamesliamr@outlook.com>
2022-02-04 14:26:59 -0800

    Finished lab

--- eggnog.s
+++ eggnog.s
@@ -1,22 +1,27 @@
     .global	main
 main:
-    // pre-increment the stack point to create space for two 8-byte registers
-    // link register (x30), and frame pointer (x29), and store them.
+
 	stp	x29, x30, [sp, -16]!
 
-    // Load the argument and perform the call. Like 'printf("...")' in C.
-	ldr	x0, =message
+
+    ldr	x0, =line1
 	bl	printf
 
-    // initialize the return value in the return register
+    ldr x0, =line2
+    bl  printf
+    
+    ldr x0, =line3
+    bl  printf
+
 	mov	x0, 0
 
-    // restore the registers and post-decrement 
-    // the stack pointer for consistency
 	ldp	x29, x30, [sp], 16
 
-    // return from the call
 	ret
 
-message:
-	.asciz "Hello, world!\n"
+line1:
+	.asciz "Egg nog is the best\n"
+line2: 
+    .asciz "Egg nog better than presents\n"
+line3: 
+    .asciz "Delicous ichor\n" 
